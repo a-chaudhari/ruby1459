417e43120f7345fe91cd2075a1eb02b3fab08bd2
Amit Chaudhari <github@amitchaudhari.com>
2017-05-05 17:32:41 -0400

    added event for forced new nickname

--- handlers/nickname.rb
+++ handlers/nickname.rb
@@ -1,5 +1,7 @@
 def ERR_NICKNAMEINUSE(chunks, raw)
+  old_nick = @nickname
   @nickname += '_'
   self.write("NICK #{@nickname}")
+  emit(:self_new_nickname)
   # self.write("USER #{@username} * * :#{@realname}")
 end
