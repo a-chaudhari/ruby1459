d74d63a055d8aa095fe620335d202db9004531ce
Amit Chaudhari <github@amitchaudhari.com>
2017-05-07 18:39:48 -0400

    fixed ipv6 support for privmsg

--- handlers/privmsg.rb
+++ handlers/privmsg.rb
@@ -1,12 +1,15 @@
 def privmsg(chunks, raw)
+  chunks = raw.split(' ')
   target = chunks[2]
   if target == @nickname
     handle_query(chunks, raw)
   else
     chan = @channels[target]
     user_str = chunks[0]
-    user = user_str.split('!',2).first
-    msg=raw.split(':',3).last
+    user_str[0]=''
+    user = user_str.split('!', 2).first
+    msg=raw.split(':', 3).last
+    msg[0] = ''
     chan._recv(:chanmsg,
                   { user: user,
                   channel: chan.channel,
@@ -18,7 +21,9 @@ end
 
 def handle_query(chunks, raw)
   user_str = chunks[0]
+  user_str[0] = ''
   msg=raw.split(':',3).last
+  msg[0] = ''
   user = user_str.split('!', 2).first
   emit(:query,{ user: user,
                 user_str: user_str,
