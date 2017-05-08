d8fd871a2af8d4b13deacbd28eea7834355961dc
Amit Chaudhari <github@amitchaudhari.com>
2017-05-07 20:00:35 -0400

    fixed clipped strings

--- handlers/privmsg.rb
+++ handlers/privmsg.rb
@@ -6,10 +6,10 @@ def privmsg(chunks, raw)
   else
     chan = @channels[target]
     user_str = chunks[0]
-    user_str[0]=''
+    user_str[0]='' if user_str[0] == ':'
     user = user_str.split('!', 2).first
     msg=raw.split(':', 3).last
-    msg[0] = ''
+    msg[0] = '' if msg[0] == ':'
     chan._recv(:chanmsg,
                   { user: user,
                   channel: chan.channel,
@@ -21,9 +21,9 @@ end
 
 def handle_query(chunks, raw)
   user_str = chunks[0]
-  user_str[0] = ''
+  user_str[0] = '' if user_str[0] == ":"
   msg=raw.split(':',3).last
-  msg[0] = ''
+  msg[0] = '' if msg[0] == ':'
   user = user_str.split('!', 2).first
   emit(:query,{ user: user,
                 user_str: user_str,
