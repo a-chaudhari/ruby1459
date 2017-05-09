9c558f71a25bb2ba6d8032e87c24ad58966c507a
Amit Chaudhari <github@amitchaudhari.com>
2017-05-09 01:05:50 -0400

    improved emote handling

--- handlers/privmsg.rb
+++ handlers/privmsg.rb
@@ -1,37 +1,55 @@
 def PRIVMSG(chunks, raw)
   target = chunks[2]
-
-  if target == @nickname
-    handle_query(chunks, raw)
-  else
-    chan = @channels[target]
-    user_str = chunks[0]
-    user = user_str.split('!', 2).first
-    msg = chunks.drop(3).join(' ')
-    emote = msg[0] == "\001"
-
-    command = { user: user,
-                channel: chan.channel,
-                user_str: user_str,
-                msg: msg,
-                emote: emote,
-                timestamp: Time.now }
-
-    chan._recv(:chanmsg, command)
-  end
-end
-
-def handle_query(chunks, raw)
+  chan = @channels[target]
+  query = target == @nickname
+  channel = query ? nil : chan.channel
   user_str = chunks[0]
-  msg = chunks.drop(3).join(' ')
   user = user_str.split('!', 2).first
-  emote = msg[0] == "\001"
+  msg = chunks.drop(3).join(' ')
+
+  emote = false
+  if msg[0] == "\001"
+    ctcp_payload = msg.split("\001").last
+    chunks = ctcp_payload.split(' ')
+    if chunks[0].casecmp('ACTION') == 0
+      emote = true
+      msg = chunks.drop(1).join(' ')
+    end
+  end
 
   command = { user: user,
+              channel: channel,
               user_str: user_str,
               msg: msg,
               emote: emote,
               timestamp: Time.now }
 
-  emit(:query, command)
+  if query
+    emit(:query, command)
+  else
+    chan._recv(:chanmsg, command)
+  end
+
 end
+
+# def handle_query(chunks, raw)
+#   user_str = chunks[0]
+#   msg = chunks.drop(3).join(' ')
+#   user = user_str.split('!', 2).first
+#   ctcp = msg[0] == "\001"
+#   emote = false
+#   if ctcp
+#     ctcp_payload = msg.split("\001").last
+#     chunks = ctcp_payload.split(' ')
+#     if chunks[0].casecmp('ACTION') == 0
+#       emote = true
+#     end
+#   end
+#   command = { user: user,
+#               user_str: user_str,
+#               msg: msg,
+#               emote: emote,
+#               timestamp: Time.now }
+#
+#   emit(:query, command)
+# end
